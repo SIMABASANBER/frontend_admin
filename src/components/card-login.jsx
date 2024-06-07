9a0fd6f1879fe001b1e8a42b3cc08dc69452e34a
irwanadinata <irwanadinata9@gmail.com>
2024-06-07 10:54:51 +0700

    refactor: card login

--- src/components/card-login.jsx
+++ src/components/card-login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Label } from "@/components/ui/label";
+import simabaLogo from "@/assets/logo/simabalogo.png"
 
 export const LayoutLogin = ({ children, label, id, route }) => {
   return (
@@ -8,13 +9,10 @@ export const LayoutLogin = ({ children, label, id, route }) => {
       <div className="w-[30.5rem] bg-white">
         <div className="flex flex-col items-center pt-2 bg-[#F4F6F9]">
           <img
-            src="/logo-new.svg"
+            src={simabaLogo}
             alt="logo"
-            className="w-[4.75rem] h-[4.375rem]"
+            className="w-[9.75rem] h-[10.375rem]"
           />
-          <p className="font-normal mb-[1.5rem] text-2xl text-[#293066]">
-            <span className="font-bold">RAIH</span> PEDULI
-          </p>
         </div>
         <hr className="border-2 border-[#293066] w-full" />
         <div className="px-8 py-8">
@@ -23,11 +21,6 @@ export const LayoutLogin = ({ children, label, id, route }) => {
             to={route}
             className="flex items-center gap-[0.625rem] mb-[1.25rem] text-[#293066]"
           >
-            <img
-              src="/left-arrow.svg"
-              alt="btn-back"
-              className="h-[1.5rem] w-[1.5rem]"
-            />
             <Label htmlFor={id} className="text-xl font-bold cursor-pointer">
               {label}
             </Label>
