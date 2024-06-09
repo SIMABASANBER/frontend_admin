c9b7cad674a6185ad1dd7cc9a648b27b4e57b31f
irwanadinata <irwanadinata9@gmail.com>
2024-06-10 00:42:55 +0700

    refactor: api user

--- src/utils/api/user/index.js
+++ src/utils/api/user/index.js
@@ -1,4 +1,4 @@
-import { getTotaUser, getUsers, getDetailUser, editUser, deleteUser, } from "./api";
+import { getUsers, getUserById, addUser, editUser, deleteUser  } from "./api";
 import { userSchema } from "./schema";
 
-export {getTotaUser, getUsers, getDetailUser, editUser, deleteUser, userSchema}
+export {getUsers, getUserById, addUser, editUser, deleteUser, userSchema}
