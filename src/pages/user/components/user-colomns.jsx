4678f260824b58067b08e720a0cb8cdea40576d7
irwanadinata <irwanadinata9@gmail.com>
2024-06-09 16:06:52 +0700

    chore: slicing user page

---
+++ src/pages/user/components/user-colomns.jsx
@@ -0,0 +1,57 @@
+import { Link } from "react-router-dom";
+import PencilIcon from "@/assets/icon/pencil";
+import { Button } from "@/components/ui/button";
+import ButtonDelete from "@/pages/user/components/alert-dialog";
+
+export const columns = [
+  {
+    header: "No",
+    accessorFn: (originalRow, index) => index + 1,
+  },
+  {
+    header: "Nama Lengkap",
+    accessorKey: "fullname",
+  },
+  {
+    header: "Username",
+    accessorKey: "username",
+  },
+  {
+    header: "Email",
+    accessorKey: "email",
+  },
+  {
+    header: "Asal Sekolah",
+    accessorKey: "from_school",
+  },
+  {
+    header: "Tahun Lulus",
+    accessorKey: "graduation_year",
+  },
+  {
+    header: "Verifikasi",
+    accessorKey: "isVerified",
+  },
+  {
+    header: "Aksi",
+    cell: ({ row }) => {
+      const id = row.original.id;
+
+      return (
+        <div className="flex gap-2">
+          <Button
+            asChild
+            size="icon"
+            className="bg-[#E28100] hover:bg-[#E28100]/80"
+            style={{ boxShadow: "0px 4px 4px 0px #00000040" }}
+          >
+            <Link to={`/user/${id}?edit=true`}>
+              <PencilIcon className="w-4 h-4" />
+            </Link>
+          </Button>
+          <ButtonDelete id={id} />
+        </div>
+      );
+    },
+  },
+];
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,16 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Login from "@/pages/login";
+import User from "@/pages/user";
 
 export default function Router() {
   const router = createBrowserRouter([
         {
           path: "/",
           element: <Login />,
+        },
+        {
+          path: "/user",
+          element: <User />,
         },
   ]);
 
