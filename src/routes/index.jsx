13583079652b3851227d802834cc0c22042125fe
irwanadinata <irwanadinata9@gmail.com>
2024-06-10 00:42:35 +0700

    refactor: routing

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -15,15 +15,23 @@ export default function Router() {
           element: <User />,
         },
         {
-          path: "/user",
-          element: <User />,
-        },
-        {
           path: "/user/:id",
           element: <DetailUser />,
         },
         {
           path: "/user/create-user",
+          element: <AddUser />,
+        },
+        {
+          path: "/questions",
+          element: <User />,
+        },
+        {
+          path: "/question/:id",
+          element: <DetailUser />,
+        },
+        {
+          path: "/questions/create-question",
           element: <AddUser />,
         },
   ]);
